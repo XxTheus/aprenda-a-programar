d79d9b7f3cff1706951fbda467352722e756158e
Matheus <matheusdearaujo1209@gmail.com>
2022-08-25 00:06:49 -0300

    Agência de turismo

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,6 +1,6 @@
-let seuNome = "Matheus"
-let seuEmail = "matheusdearaujo1209@gmail.com"
-let suaProfissao = "Estudante"
-let suaIdade = 17
+let nome = "Matheus"
+let email = "matheusdearaujo1209@gmail.com"
+let profissao = "Estudante"
+let idade = 17
 let local = "EUA"
 console.log("Olá, "+ seuNome +"! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para "+ local +". Confirma pra gente alguns dados? A sua idade é "+ suaIdade +", você é "+ suaProfissao +" e o seu email é "+ seuEmail +"? Ficamos no aguardo. Muito obrigado e boa viagem! :D")

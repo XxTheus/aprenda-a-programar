5281105463492d1369a2469604b4580612fe676b
Matheus <matheusdearaujo1209@gmail.com>
2022-08-25 20:49:05 -0300

    Viagem

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,4 +3,4 @@ let email = "matheusdearaujo1209@gmail.com"
 let profissao = "Estudante"
 let idade = 17
 let local = "EUA"
-console.log("Olá, "+ seuNome +"! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para "+ local +". Confirma pra gente alguns dados? A sua idade é "+ suaIdade +", você é "+ suaProfissao +" e o seu email é "+ seuEmail +"? Ficamos no aguardo. Muito obrigado e boa viagem! :D")
+console.log("Olá, "+ nome +"! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para "+ local +". Confirma pra gente alguns dados? A sua idade é "+ idade +", você é "+ profissao +" e o seu email é "+ email +"? Ficamos no aguardo. Muito obrigado e boa viagem! :D")

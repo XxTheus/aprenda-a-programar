d587930c36181caca2ebfac3a34e1f0edd4ed4cc
Matheus <matheusdearaujo1209@gmail.com>
2022-08-26 22:30:19 -0300

    Qual número é primo?

---
+++ tarefa-dia-3/pratica-4.js
@@ -0,0 +1,9 @@
+let numero = 11
+let i = 2
+while (numero % i != 0)
+    i++
+    
+if (numero == i)
+    console.log(`Sim, o número ${numero} é primo.`)
+else
+    console.log(`Não, o número ${numero} não é primo.`)

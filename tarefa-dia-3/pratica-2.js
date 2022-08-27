9bab146f4aaf1cc19ec1fa9e1649e5fe7bbef47b
Matheus <matheusdearaujo1209@gmail.com>
2022-08-26 21:02:02 -0300

    Tabuada

--- tarefa-dia-3/pratica-2.js
+++ tarefa-dia-3/pratica-2.js
@@ -1,6 +1,6 @@
-let multiplicador = 9
+let multiplicador = 9 //Ex: {multiplicador} X {i}
 let i = 1
-let n = 10
+let n = 10 //Nº de vezes multiplicado
 
 while (i <= n) {
     console.log(`${multiplicador} X ${i} = ${multiplicador * i}`)
